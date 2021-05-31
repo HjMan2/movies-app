98809374feb4cf32556d9d0427fc0a86b6b50eae
hosseinJS <79367122+hosseinJS@users.noreply.github.com>
2021-05-31 13:55:48 +0430

    clean up code and refactor methods with api

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -1,6 +1,6 @@
 import Form from './common/form'
-import { getGenres } from '../services/fakeGenreService'
-import { getMovie, saveMovie } from '../services/fakeMovieService'
+import { getGenres } from '../services/genreService'
+import { getMovie, saveMovie } from '../services/movieService'
 import Joi from 'joi-browser'
 class MovieForm extends Form {
     state = {
@@ -21,24 +21,29 @@ class MovieForm extends Form {
         numberInStock: Joi.number().required().min(0).max(100),
         dailyRentalRate: Joi.number().required().min(0).max(10)
     }
-    componentDidMount() {
-        const genres = getGenres()
+
+    async populateGenres() {
+        const { data: genres } = await getGenres()
         this.setState({genres})
-        const movieId = this.props.match.params.id
-        if(movieId === 'new') return;
-        const movie = getMovie(movieId)
-        if(!movie) return this.props.history.replace('/not-found')
-        this.setState({data: this.mapToViewModel(movie)})
+    }
+    async populateMovie() {
+        try{
+            const movieId = this.props.match.params.id
+            if(movieId === 'new') return;
+            const { data: movie } = await getMovie(movieId)
+            this.setState({data: this.mapToViewModel(movie)})
+        } catch(error) {
+            if(error.response && error.response.status === 404)
+                this.props.history.replace('/not-found')
+        }
+    }
+
+    async componentDidMount() {
+        await this.populateGenres()
+        await this.populateMovie()
     }
 
     mapToViewModel(movie) {
-        console.log({
-            _id: movie._id,
-            title: movie.title,
-            genreId: movie.genre._id,
-            numberInStock: movie.numberInStock,
-            dailyRentalRate: movie.dailyRentalRate
-        })
         return {
             _id: movie._id,
             title: movie.title,
@@ -48,8 +53,8 @@ class MovieForm extends Form {
         }
     }
 
-    doSubmit = () => {
-        saveMovie(this.state.data)
+    doSubmit = async () => {
+        await saveMovie(this.state.data)
         this.props.history.push('/movies')
     }
 
